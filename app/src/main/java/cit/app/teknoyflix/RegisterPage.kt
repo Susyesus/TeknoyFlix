584ddeb68d6052ed5f263f408b124dc738b50e77
Hikken <kenapor19@gmail.com>
2025-04-03 20:53:30 +0800

    second commit

--- app/src/main/java/cit/app/teknoyflix/RegisterPage.kt
+++ app/src/main/java/cit/app/teknoyflix/RegisterPage.kt
@@ -10,6 +10,7 @@ import android.widget.EditText
 import android.widget.Toast
 
 class RegisterPage : Activity() {
+    //ken gwapo
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
